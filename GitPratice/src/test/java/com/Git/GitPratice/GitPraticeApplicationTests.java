6ae7be155e7f6fd7a00b899643ac8e92ef6dfcae
sudheer <sudheeryedlapalli117@gmail.com>
2026-05-31 22:21:08 -0700

    Sudheer First Commit

---
+++ GitPratice/src/test/java/com/Git/GitPratice/GitPraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Git.GitPratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPraticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
